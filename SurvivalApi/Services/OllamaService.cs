9a22e95ae0380d6bb7254b87badc2e84feca2f56
argentinamoose <argentinamoose@gmail.com>
2024-10-21 00:55:16 -0500

    Json Parsing

--- SurvivalApi/Services/OllamaService.cs
+++ SurvivalApi/Services/OllamaService.cs
@@ -3,6 +3,22 @@ using System.Text.Json;
 
 namespace SurvivalApi.Services;
 
+public class ResponseType
+{
+    public String? model { get; set; }
+    public String? created_at { get; set; }
+    public String? response { get; set; }
+    public bool done { get; set; }
+    /*public String? done_reason { get; set; }
+    public Int64[]? context { get; set; }
+    public int total_duration { get; set; }
+    public int load_duration { get; set; }
+    public int prompt_eval_count { get; set; }
+    public Int64 prompt_eval_duration { get; set; }
+    public int eval_count { get; set; }
+    public Int64 eval_duration { get; set; }
+    */
+}
 
 public class OllamaPayload
 {
@@ -20,18 +36,26 @@ public class Options
 public class OllamaService
 {
     
-    public async Task<int> SendToOllama(String prompt) {
+    public async Task<String> SendToOllama(String prompt) {
         HttpClient client = new HttpClient();
         var toSerialize = new OllamaPayload{
             prompt = prompt
         };
         var payload = JsonSerializer.Serialize(toSerialize);
-        Console.WriteLine(payload);
         var res = await client.PostAsJsonAsync(
             "http://localhost:11434/api/generate", toSerialize); 
         var answer = res.Content.ReadAsStringAsync().Result;
-        Console.WriteLine(answer);
-        return 1;
+        var split = "done_reason";
+        if (answer.IndexOf(split) != -1){
+            string substring = answer.Substring(0, answer.IndexOf(split) - 2) + "}";
+            var answerJson = JsonSerializer.Deserialize<ResponseType>(substring);
+            var finalAnswer = answerJson.response;
+            return finalAnswer;
+        }
+        else {
+            Console.WriteLine("There was an error with the model");
+            return "There is a fire in the building";
+        }
     }
 
     public String CleanOllamaResponse(String res) {
@@ -39,7 +63,7 @@ public class OllamaService
     }
 
     public async Task CreateScenario(String[] playerNames) {
-        var standardPrompt = "create a dangerous scenario involving the following people:";
+        var standardPrompt = "create one dangerous scenario involving the following people:";
         var playersString = string.Join(" ", playerNames);
         var prompt = standardPrompt + playersString;
         await SendToOllama(prompt);
@@ -52,7 +76,8 @@ public class OllamaService
     }
 
     public async Task TestSendToOllama() {
-        var standardPrompt = "create a dangerous scenario involving the following people:";
-        await SendToOllama(standardPrompt);
+        var standardPrompt = "create one dangerous scenario, make it under 200 characters";
+        var prompt = await SendToOllama(standardPrompt);
+        Console.WriteLine(prompt);
     }
 }
