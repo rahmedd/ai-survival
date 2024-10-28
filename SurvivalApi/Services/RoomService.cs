a75e8e5f9f1af3bbc4b326afe8a6da17a52d36fa
raed <raed>
2024-10-27 20:15:57 -0500

    ensure players or connections to rooms arent duplicated

--- SurvivalApi/Services/RoomService.cs
+++ SurvivalApi/Services/RoomService.cs
@@ -19,18 +19,24 @@ public class RoomService
 	{
 		var db = _redis.GetDatabase();
 
+		// var playerExists = await db.KeyExistsAsync($"player:{connectionId}");
 		var gameExists = await db.KeyExistsAsync($"room:{groupName}");
 		var playersExist = await db.SetLengthAsync($"room:{groupName}:players") > 0;
 		var host = !gameExists && !playersExist;
 		
-		// if goup doesn't exist
-		if (host)
+		if (groupName == "" || groupName.Length < 6)
 		{
-			if (groupName == "" || groupName.Length < 6)
-			{
-				var faker = new Faker();
-				groupName = faker.Random.Words(4);
-			}
+			var faker = new Faker();
+			groupName = faker.Random.Words(4);
+		}
+
+		// rmeove from existing room and delete player
+		var existingPlayerRoom = await db.HashGetAsync($"player:{connectionId}", "roomId");
+		var existingPlayerRoomString = existingPlayerRoom.ToString();
+		// if (playerExists && existingPlayerRoomString != groupName)
+		if (existingPlayerRoomString.Length > 0)
+		{
+			await RemoveFromRoom(connectionId, existingPlayerRoomString);
 		}
 
 		var roomKey = $"room:{groupName}";
@@ -41,12 +47,13 @@ public class RoomService
         [
 			new HashEntry("id", connectionId),
 			new HashEntry("username", username),
+			new HashEntry("roomId", roomKey),
 			new HashEntry("health", 5),
 			new HashEntry("host", host),
 		]);
 	}
 
-	public async Task RemoveFromRoom(string connectionId, string username, string groupName)
+	public async Task RemoveFromRoom(string connectionId, string groupName)
 	{
 		var db = _redis.GetDatabase();
 
